f3f19318c0374a277ff6fe1fdc11b054c07fa354
Xavier Jackson <xavierejackson20@gmail.com>
2020-07-23 01:01:06 -0400

    added in psuedocode

--- index.js
+++ index.js
@@ -1,2 +1,67 @@
 console.log("Hello World!")
 
+// GIVEN a command-line application that accepts user input
+
+    // Creat variable than can accept user input
+    // var userInput = process.argv[2]
+
+// WHEN I am prompted for information about my application repository
+
+    // Prompt for information
+    // prompt("Hello")
+
+// THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+    // Create a function that prompts the user for information on their README
+    // and generates a README.md file with sections for Description, Table of Contents, 
+    // Installation, Usage, License, Contributing, Tests, and Questions
+
+    // var fs = require("fs")
+
+// WHEN I enter my project title
+
+    // Create variable that can store user input for projectTitle
+
+// THEN this is displayed as the title of the README
+
+    // Set the value of projectTitle and display the value in the README file where the Description section is
+
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+
+    // Create variables that can store the user input for description, installation instructions, usage information, contribution guidelines, and test instructions
+
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+    // Display the information in the corresponding part of the README
+
+// WHEN I choose a license for my application from a list of options
+
+    // Create an array with a list of options for licenses 
+
+// THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+    // ???
+
+// WHEN I enter my GitHub username
+
+    // Create variable for GitHub username and set it equal to the value of the user's input 
+
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+    // Add GitHub username to the Questions section on the README and add a link to my GitHub profile
+
+// WHEN I enter my email address
+
+    // Create a variable for email address and set it equal to the value of the user's input
+
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+    // Display the email address in the Questions section of the README with instructions on how to reach me
+
+// WHEN I click on the links in the Table of Contents
+
+    // ???
+
+// THEN I am taken to the corresponding section of the README
+
+    // ???
